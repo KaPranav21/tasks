88ecc1c2dc845def0335f585d5e266822a245ba1
pkama <pkama@udel.edu>
2022-09-02 13:08:07 -0400

    s

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript by Pranav Kamath : prnv
+            </header>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload.
+
+                <header> THIS IS PRANAV OMG :0 </header>
+
+                <p>SCRUM IS AN AGILE PROCESS FRAMEWORK FOR MANAGING</p>
+                <p> WITH AN INITIAL EMPHASIS ON SOFTWARE DEVELOPMENT</p>
+                <p> ALTHOUGH IT HAS BEEN USED IN OTHER FIELDS AND IS</p>
+                <p> SLOWLY STARTING TO BE EXPLORED FOR OTHER COMPLEX</p>
+                <p> WORK, RESEARCH AND ADVANCED TECHNOLOGIES.</p>
+                
+            </p>
+        </div>
+    );
+}
+
+export default App;
