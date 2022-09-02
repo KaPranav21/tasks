94ddeee7d70ee6c2d8987657230b0881819dcfba
pkama <pkama@udel.edu>
2022-09-02 19:14:41 -0400

    Added content to pkama files

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,11 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript BY PRANAV
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-                pranav edited this file
+                automatically reload. pranav edited this file. Hello World
             </p>
         </div>
     );
